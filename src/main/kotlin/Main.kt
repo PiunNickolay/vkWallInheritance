85b0688e3de398a8af479714fceeff3f822008dd
PiunNickolay <147736400+PiunNickolay@users.noreply.github.com>
2024-07-24 21:59:37 +0300

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,16 +15,19 @@ data class Post(
     val likes: Likes = Likes(0),
     val attachments: Array<Attachment>? = null
 )
-abstract class Attachment(val type: String)
 
+abstract class Attachment(val type: String)
 data class Photo(val id: Int, val ownerId: Int, val photo130: String, val photo604: String)
 data class PhotoAttachment(val photo: Photo) : Attachment("photo")
-
 data class Video(val id: Int, val ownerId: Int, val title: String, val duration: Int)
 data class VideoAttachment(val video: Video) : Attachment("video")
-
 data class Audio(val id: Int, val ownerId: Int, val title: String, val description: String, val duration: Int)
 data class AudioAttachment(val audio: Audio) : Attachment("audio")
+data class Doc(val id: Int, val ownerId: Int, val title: String, val size: Int, val ext: String)
+data class DocAttachment(val doc: Doc) : Attachment("doc")
+data class Link(val url: String, val title: String, val description: String)
+data class LinkAttachment(val link: Link) : Attachment("link")
+
 object WallService {
     var posts = emptyArray<Post>()
     private var nextId = 1
@@ -42,7 +45,9 @@ object WallService {
                     ownerId = storedPost.ownerId,
                     fromId = storedPost.fromId,
                     createdBy = storedPost.createdBy,
-                    date = storedPost.date
+                    date = storedPost.date,
+                    likes = storedPost.likes,
+                    attachments = storedPost.attachments
                 )
                 return true
             }
@@ -53,7 +58,19 @@ object WallService {
 
 fun main() {
     val photo = Photo(1, 1, "photo1", "photo2")
-    val attachment = PhotoAttachment(photo)
+    val photoAttachment = PhotoAttachment(photo)
+
+    val video = Video(1, 1, "Video1", 300)
+    val videoAttachment = VideoAttachment(video)
+
+    val audio = Audio(1, 1, "Audio1", "Description", 200)
+    val audioAttachment = AudioAttachment(audio)
+
+    val doc = Doc(1, 1, "Doc1", 1024, "txt")
+    val docAttachment = DocAttachment(doc)
+
+    val link = Link("https://netology.ru", "ссылка2", "Ссылка на Нетологию")
+    val linkAttachment = LinkAttachment(link)
 
     val post = Post(
         id = 0,
@@ -61,18 +78,18 @@ fun main() {
         fromId = 1,
         createdBy = 1,
         date = 20220628,
-        text = "Пост с фотографией",
+        text = "Пост с различными вложениями",
         replyOwnerId = 0,
         replyPostId = 0,
         friendsOnly = false,
         postType = "post",
-        attachments = arrayOf(attachment)
+        attachments = arrayOf(photoAttachment, videoAttachment, audioAttachment, docAttachment, linkAttachment)
     )
 
     val addedPost = WallService.add(post)
     println(addedPost)
 
-    val updatePost = addedPost.copy(text = "Обновление поста с фото")
+    val updatePost = addedPost.copy(text = "Обновленный пост с различными вложениями")
     val result = WallService.update(updatePost)
     println(result)
 }
